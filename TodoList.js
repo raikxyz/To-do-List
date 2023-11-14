04d0ca100a586688c7d727cd5ac6c285241b7cdd
Julio Cesar <141031457+raikxyz@users.noreply.github.com>
2023-11-14 11:25:10 -0300

    Add files via upload

---
+++ TodoList.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import TodoForm from "./TodoForm";
+import Todo from "./Todo";
+
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return;
+    }
+
+    const newTodos = [todo, ...todos];
+
+    setTodos(newTodos);
+
+  };
+  return (
+    <div>
+      <h1>To-do List</h1>
+      <TodoForm onSubmit={addTodo} />
+      <Todo 
+      todos={todos}
+      completeTodo={completeTodo}
+      />
+    </div>
+  );
+}
+
+export default TodoList;
