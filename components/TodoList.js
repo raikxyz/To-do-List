6a01a835fb0573ae8c8a370c0bb54fcc8a6dda4b
Julio Cesar <141031457+raikxyz@users.noreply.github.com>
2023-11-16 08:36:57 -0300

    Update TodoList.js
    Added remove and edit functions

--- components/TodoList.js
+++ components/TodoList.js
@@ -13,15 +13,43 @@ function TodoList() {
     const newTodos = [todo, ...todos];
 
     setTodos(newTodos);
-
   };
+
+  const updateTodo = (todoId, newValue) => {
+    if (!newValue.text || /^\s*$/.test(newValue.text)) {
+      return;
+    }
+
+    setTodos((prev) =>
+      prev.map((item) => (item.id === todoId ? newValue : item))
+    );
+  };
+
+  const removeTodo = (id) => {
+    const removeArr = [...todos].filter((todo) => todo.id !== id);
+
+    setTodos(removeArr);
+  };
+
+  const completeTodo = (id) => {
+    let updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
   return (
     <div>
       <h1>To-do List</h1>
       <TodoForm onSubmit={addTodo} />
-      <Todo 
-      todos={todos}
-      completeTodo={completeTodo}
+      <Todo
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        updateTodo={updateTodo}
       />
     </div>
   );
